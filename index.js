74891ef44106e876e5f545f305f4319a3c5fa3ac
harilesmana <harilesmana878@gmail.com>
2025-06-04 11:15:37 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// LOOPING PERTAMA (maju)
+console.log('LOOPING PERTAMA');
+for (let i = 20; i >= 2; i -= 2) {
+  console.log(`${i} - I love coding`);
+}
+
+// LOOPING KEDUA (mundur)
+console.log('LOOPING KEDUA');
+for (let i = 20; i >= 2; i -= 2) {
+  console.log(`${i} - I will become a fullstack developer`);
+}
